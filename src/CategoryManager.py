5df81bea153040c5fd2afe1d6b3ab551b93a7204
unknown <zhangwenkang@gmail.com>
2025-05-21 09:46:23 +0800

    类别管理优化，增加拖动、设置特殊类别

--- src/CategoryManager.py
+++ src/CategoryManager.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import (QDialog, QVBoxLayout, QHBoxLayout, QListWidget,   
                              QPushButton, QInputDialog, QMessageBox,   
-                             QColorDialog, QSplitter, QWidget, QLabel, QDesktopWidget)  
+                             QColorDialog, QSplitter, QWidget, QLabel, QDesktopWidget,
+                             QMenu)  
 from PyQt5.QtGui import QColor  
 from PyQt5.QtCore import Qt  
 from .DictManage import *  
@@ -31,31 +32,25 @@ class CategoryManager(QDialog, Observer):
         leftWidget = QWidget()  
         leftLayout = QVBoxLayout(leftWidget)  
 
-        self.categoryList = QListWidget()  
+        self.categoryList = QListWidget()
+        # 启用拖放功能
+        self.categoryList.setDragDropMode(QListWidget.InternalMove)
+        self.categoryList.setDefaultDropAction(Qt.MoveAction)
+        self.categoryList.model().rowsMoved.connect(self.onCategoryOrderChanged)
+        # 右键菜单
+        self.categoryList.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.categoryList.customContextMenuRequested.connect(self.showCategoryContextMenu)
         leftLayout.addWidget(self.categoryList)  
 
-        # 类别按钮 - 第一行  
-        buttonLayout1 = QHBoxLayout()  
-        self.addButton = QPushButton("添加类别")  
-        self.editButton = QPushButton("编辑类别")  
-        self.deleteButton = QPushButton("删除类别")  
-        self.colorButton = QPushButton("设置颜色")  
-
-        buttonLayout1.addWidget(self.addButton)  
-        buttonLayout1.addWidget(self.editButton)  
-        buttonLayout1.addWidget(self.deleteButton)  
-        buttonLayout1.addWidget(self.colorButton)  
-
-        # 类别按钮 - 第二行  
-        buttonLayout2 = QHBoxLayout()  
-        self.upCategoryButton = QPushButton("上移类别")  
-        self.downCategoryButton = QPushButton("下移类别")  
-
-        buttonLayout2.addWidget(self.upCategoryButton)  
-        buttonLayout2.addWidget(self.downCategoryButton)  
-
-        leftLayout.addLayout(buttonLayout1)  
-        leftLayout.addLayout(buttonLayout2)  
+        # 类别操作按钮：添加/上移/下移
+        buttonLayout = QHBoxLayout()  
+        self.addButton = QPushButton("添加类别")
+        self.upCategoryButton = QPushButton("上移")  
+        self.downCategoryButton = QPushButton("下移")
+        buttonLayout.addWidget(self.addButton)
+        buttonLayout.addWidget(self.upCategoryButton)  
+        buttonLayout.addWidget(self.downCategoryButton)
+        leftLayout.addLayout(buttonLayout)  
 
         # 右侧：标签列表和按钮  
         rightWidget = QWidget()  
@@ -64,25 +59,25 @@ class CategoryManager(QDialog, Observer):
         self.tagListLabel = QLabel("标签列表")  
         rightLayout.addWidget(self.tagListLabel)  
 
-        self.tagList = QListWidget()  
+        self.tagList = QListWidget()
+        # 启用拖放功能
+        self.tagList.setDragDropMode(QListWidget.InternalMove)
+        self.tagList.setDefaultDropAction(Qt.MoveAction)
+        self.tagList.model().rowsMoved.connect(self.onTagOrderChanged)
+        # 右键菜单
+        self.tagList.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.tagList.customContextMenuRequested.connect(self.showTagContextMenu)
         rightLayout.addWidget(self.tagList)  
 
-        # 标签按钮 - 第一行  
-        tagButtonLayout1 = QHBoxLayout()  
-        self.addTagButton = QPushButton("添加标签")  
-        self.removeTagButton = QPushButton("移除标签")  
-        tagButtonLayout1.addWidget(self.addTagButton)  
-        tagButtonLayout1.addWidget(self.removeTagButton)  
-
-        # 标签按钮 - 第二行  
-        tagButtonLayout2 = QHBoxLayout()  
-        self.upTagButton = QPushButton("上移标签")  
-        self.downTagButton = QPushButton("下移标签")  
-        tagButtonLayout2.addWidget(self.upTagButton)  
-        tagButtonLayout2.addWidget(self.downTagButton)  
-
-        rightLayout.addLayout(tagButtonLayout1)  
-        rightLayout.addLayout(tagButtonLayout2)  
+        # 标签操作按钮：添加/上移/下移
+        tagButtonLayout = QHBoxLayout()  
+        self.addTagButton = QPushButton("添加标签")
+        self.upTagButton = QPushButton("上移")  
+        self.downTagButton = QPushButton("下移")
+        tagButtonLayout.addWidget(self.addTagButton)
+        tagButtonLayout.addWidget(self.upTagButton)  
+        tagButtonLayout.addWidget(self.downTagButton)
+        rightLayout.addLayout(tagButtonLayout)  
 
         # 使用QSplitter来允许用户调整左右两侧的宽度  
         splitter = QSplitter(Qt.Horizontal)  
@@ -93,20 +88,79 @@ class CategoryManager(QDialog, Observer):
         self.setLayout(layout)  
 
         # 连接信号和槽  
-        self.addButton.clicked.connect(self.addCategory)  
-        self.editButton.clicked.connect(self.editCategory)  
-        self.deleteButton.clicked.connect(self.deleteCategory)  
-        self.colorButton.clicked.connect(self.setColor)  
-        self.categoryList.currentItemChanged.connect(self.onCategoryChanged)  
-        self.addTagButton.clicked.connect(self.addTag)  
-        self.removeTagButton.clicked.connect(self.removeTag)  
+        self.addButton.clicked.connect(self.addCategory)
         self.upCategoryButton.clicked.connect(self.upMoveCategory)  
-        self.downCategoryButton.clicked.connect(self.downMoveCategory)  
+        self.downCategoryButton.clicked.connect(self.downMoveCategory)
+        self.categoryList.currentItemChanged.connect(self.onCategoryChanged)  
+        self.addTagButton.clicked.connect(self.addTag) 
         self.upTagButton.clicked.connect(self.upMoveTag)  
-        self.downTagButton.clicked.connect(self.downMoveTag)  
-        
-        # 添加标签选择变化跟踪  
+        self.downTagButton.clicked.connect(self.downMoveTag)
         self.tagList.currentItemChanged.connect(self.onTagChanged)  
+
+    def showCategoryContextMenu(self, position):
+        menu = QMenu()
+        currentItem = self.categoryList.currentItem()
+        
+        if currentItem:
+            editAction = menu.addAction("编辑类别")
+            deleteAction = menu.addAction("删除类别")
+            colorAction = menu.addAction("设置颜色")
+            
+            # 根据特殊类别状态动态设置文本
+            category = currentItem.text()
+            specialText = "取消特殊类别" if category in self.DictManage.special_categories else "设为特殊类别"
+            specialAction = menu.addAction(specialText)
+            
+            # 获取用户点击的操作
+            action = menu.exec_(self.categoryList.mapToGlobal(position))
+            
+            # 根据用户选择执行相应操作
+            if action == editAction:
+                self.editCategory()
+            elif action == deleteAction:
+                self.deleteCategory()
+            elif action == colorAction:
+                self.setColor()
+            elif action == specialAction:
+                self.setSpecialCategory()
+
+    def showTagContextMenu(self, position):
+        menu = QMenu()
+        currentTag = self.tagList.currentItem()
+        
+        if currentTag and self.current_category:
+            removeAction = menu.addAction("移除标签")
+            
+            action = menu.exec_(self.tagList.mapToGlobal(position))
+            
+            if action == removeAction:
+                self.removeTag()
+
+    def onCategoryOrderChanged(self, parent, start, end, destination, row):
+        # 当用户拖动完成后更新数据模型
+        categories = [self.categoryList.item(i).text() for i in range(self.categoryList.count())]
+        self.reorderCategories(categories)
+    
+    def reorderCategories(self, new_order):
+        # 重新排序类别
+        new_categories = {}
+        for category in new_order:
+            if category in self.relation_graph['category']:
+                new_categories[category] = self.relation_graph['category'][category]
+        
+        # 处理可能未包含在new_order中的类别
+        for category in self.relation_graph['category']:
+            if category not in new_categories:
+                new_categories[category] = self.relation_graph['category'][category]
+        
+        self.relation_graph['category'] = new_categories
+        self.saveCategories()
+    
+    def onTagOrderChanged(self):
+        if not self.current_category:
+            return
+        tags = [self.tagList.item(i).text() for i in range(self.tagList.count())]
+        self.DictManage.reorder_tags(self.current_category, tags)
 
     def center(self):  
         # 获取屏幕几何信息  
@@ -128,11 +182,9 @@ class CategoryManager(QDialog, Observer):
         self.loadCategories()  
         # 恢复选择状态  
         if self.current_category:  
-            # 查找并选择之前选中的类别  
             items = self.categoryList.findItems(self.current_category, Qt.MatchExactly)  
             if items:  
-                self.categoryList.setCurrentItem(items[0])  
-                # 当类别被选中后，onCategoryChanged会处理标签的恢复  
+                self.categoryList.setCurrentItem(items[0])
 
     def loadCategories(self):  
         self.categoryList.clear()  
@@ -211,6 +263,20 @@ class CategoryManager(QDialog, Observer):
                 self.relation_graph['category'][category]['tagColor'] = color.name()  
                 self.saveCategories()  
 
+    def setSpecialCategory(self):
+        currentItem = self.categoryList.currentItem()
+        if currentItem:
+            category = currentItem.text()
+            if category in self.DictManage.special_categories:
+                self.DictManage.special_categories.remove(category)
+            else:
+                self.DictManage.special_categories.append(category)
+            
+            # 保存更改
+            with shelve.open(self.DictManage.tag_dict_path, writeback=True) as shelf:
+                shelf['special_categories'] = self.DictManage.special_categories
+            self.DictManage.save_notify()
+
     def cherk_tag(self, tag):
         operators = [' ∩ ', ' ∪ ', "'", '(', ')', '-']
         for op in operators:
@@ -259,41 +325,49 @@ class CategoryManager(QDialog, Observer):
                 self.DictManage.change_tag_category(tag, '未分类')  
                 self.onCategoryChanged(category, None)  
 
-    def upMoveTag(self):  
-        currentCategory = self.categoryList.currentItem()  
-        currentTag = self.tagList.currentItem()  
-        if currentCategory and currentTag:  
-            category = currentCategory.text()  
-            tag = currentTag.text()  
-            self.current_category = category  # 记住当前选择的类别  
-            self.current_tag = tag  # 记住当前选择的标签  
-            list_object = self.relation_graph['category'][category]['tagOrder']  
-            self.DictManage.move_element(list_object, tag, 'up')  
-    
-    def downMoveTag(self):  
-        currentCategory = self.categoryList.currentItem()  
-        currentTag = self.tagList.currentItem()  
-        if currentCategory and currentTag:  
-            category = currentCategory.text()  
-            tag = currentTag.text()  
-            self.current_category = category  # 记住当前选择的类别  
-            self.current_tag = tag  # 记住当前选择的标签  
-            list_object = self.relation_graph['category'][category]['tagOrder']  
-            self.DictManage.move_element(list_object, tag, 'down')  
+    def upMoveTag(self):
+        currentTag = self.tagList.currentItem()
+        if currentTag and self.current_category:
+            current_index = self.tagList.row(currentTag)
+            if current_index > 0:
+                tags = [self.tagList.item(i).text() for i in range(self.tagList.count())]
+                tags[current_index], tags[current_index-1] = tags[current_index-1], tags[current_index]
+                self.DictManage.reorder_tags(self.current_category, tags)
+                # 保持选中状态
+                self.tagList.setCurrentRow(current_index-1)
+
+    def downMoveTag(self):
+        currentTag = self.tagList.currentItem()
+        if currentTag and self.current_category:
+            current_index = self.tagList.row(currentTag)
+            if current_index < self.tagList.count() - 1:
+                tags = [self.tagList.item(i).text() for i in range(self.tagList.count())]
+                tags[current_index], tags[current_index+1] = tags[current_index+1], tags[current_index]
+                self.DictManage.reorder_tags(self.current_category, tags)
+                # 保持选中状态
+                self.tagList.setCurrentRow(current_index+1)
 
     def upMoveCategory(self):  
-        currentCategory = self.categoryList.currentItem()  
-        if currentCategory:  
-            category = currentCategory.text()  
-            self.current_category = category  # 记住当前选择的类别  
-            self.DictManage.move_key(self.relation_graph['category'], category, 'up')  
+        currentItem = self.categoryList.currentItem()  
+        if currentItem:  
+            current_index = self.categoryList.row(currentItem)
+            if current_index > 0:
+                categories = [self.categoryList.item(i).text() for i in range(self.categoryList.count())]
+                categories[current_index], categories[current_index-1] = categories[current_index-1], categories[current_index]
+                self.reorderCategories(categories)
+                # 保持选中状态
+                self.categoryList.setCurrentRow(current_index-1)
     
     def downMoveCategory(self):  
-        currentCategory = self.categoryList.currentItem()  
-        if currentCategory:  
-            category = currentCategory.text()  
-            self.current_category = category  # 记住当前选择的类别  
-            self.DictManage.move_key(self.relation_graph['category'], category, 'down')  
+        currentItem = self.categoryList.currentItem()  
+        if currentItem:  
+            current_index = self.categoryList.row(currentItem)
+            if current_index < self.categoryList.count() - 1:
+                categories = [self.categoryList.item(i).text() for i in range(self.categoryList.count())]
+                categories[current_index], categories[current_index+1] = categories[current_index+1], categories[current_index]
+                self.reorderCategories(categories)
+                # 保持选中状态
+                self.categoryList.setCurrentRow(current_index+1)
 
     def saveCategories(self):  
-        self.DictManage.save_notify()  
+        self.DictManage.save_notify()
